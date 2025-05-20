1ac14a3413d19458866839a6b7eeed6db7a94bc5
Malala Ravalisaona <malala.9@live.fr>
2025-05-20 13:55:40 +0200

    dfg

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import 'dotenv/config'
 import Fastify from 'fastify'
 import { submitForReview } from './submission.js'
+import { cityRoutes } from './cities.js'
 
-const fastify = Fastify({
-  logger: true,
-})
+const fastify = Fastify({ logger: true })
 
-fastify.listen(
-  {
-    port: process.env.PORT || 3000,
-    host: process.env.RENDER_EXTERNAL_URL ? '0.0.0.0' : process.env.HOST || 'localhost',
-  },
-  function (err) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
+// Fonction main auto-invoquée pour utiliser await
+async function main() {
+  await fastify.register(cityRoutes)
+
+  fastify.listen(
+    {
+      port: process.env.PORT || 3000,
+      host: process.env.RENDER_EXTERNAL_URL ? '0.0.0.0' : process.env.HOST || 'localhost',
+    },
+    function (err) {
+      if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+      }
+
+      // Soumission pour validation
+      submitForReview(fastify)
     }
+  )
+}
 
-    //////////////////////////////////////////////////////////////////////
-    // Don't delete this line, it is used to submit your API for review //
-    // everytime your start your server.                                //
-    //////////////////////////////////////////////////////////////////////
-    submitForReview(fastify)
-  }
-)
+main().catch((err) => {
+  fastify.log.error(err)
+  process.exit(1)
+})
